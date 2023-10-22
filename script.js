71d5c9a452ec97904a8d41544d71212361e20846
Bader Shamlan <98660242+BwShamlan@users.noreply.github.com>
2023-10-22 13:21:19 +0300

    JS file uploaded

---
+++ script.js
@@ -0,0 +1,40 @@
+const initLikes = 220;
+const initDisLikes = 34;
+
+let likesCount = initLikes
+let dislikesCount = initDisLikes
+
+let LikeBtn = document.getElementById("likeBtn");
+let disLikeBtn = document.getElementById("dislikeBtn");
+
+likeBtn.innerText = "👍" + likesCount
+dislikeBtn.innerText = "👎" + dislikesCount
+
+function doLike() {
+    likesCount++;
+    likeBtn.innerText = "👍 " + likesCount
+    disableButtons();
+    setCookie();
+}
+
+function doDisLike() {
+    dislikesCount++;
+    dislikeBtn.innerText = "👎 " + dislikesCount
+    disableButtons();
+    setCookie();
+}
+
+function disableButtons() {
+    likeBtn.disabled = true;
+    disLikeBtn.disabled = true;
+}
+
+function setCookie() {
+    document.cookie = "voted=true; SameSite=Strict; Max-Age=" + 60
+}
+
+window.onload = function () {
+    if (document.cookie && document.cookie.indexOf("voted") > -1) {
+        disableButtons();
+    }
+}
